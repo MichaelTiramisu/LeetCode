097399cdaa86da7d71cd3f02cc7fd10aa48b7f96
lsy <35537629@qq.com>
2017-03-07 20:36:20 -0600

    add *.db to .gitignore for Visual Studio, and initial P002

---
+++ P002_AddTwoNumbers/P002_AddTwoNumbers/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+/**
+* Definition for singly-linked list.
+*/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
